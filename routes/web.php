e3b870500a902bf9aabb45af3b3bb29bf63077ed
dimond <hirashrestha9840@gmail.com>
2025-06-20 20:44:22 +0545

    frontend setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\dashboard\DashboardController;
+use App\Http\Controllers\frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[DashboardController::class, 'index'])->name('admin.index');
@@ -14,3 +15,8 @@ Route::put('admin/update/{id}',[DashboardController::class, 'update'])->name('up
 
 //Delete client data
 Route::delete('admin/delete/{id}',[DashboardController::class, 'delete'])->name('client.delete');
+
+
+
+//frontend starts
+Route::get('home',[HomeController::class, 'index'])->name('home.index');
